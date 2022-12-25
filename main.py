7c20f094f0ee712e5bb329b6e8b92d37e26645d3
Jamshidbek <91362182+My-name-is-Jamshidbek@users.noreply.github.com>
2022-12-25 07:11:21 -0600

    hammasi tayyor

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import speech_recognition as sr
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Create a Recognizer object
+r = sr.Recognizer()
 
+# Read the audio file
+with sr.AudioFile('voice_message.wav') as source:
+    audio = r.record(source)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Transcribe the audio
+text = r.recognize_google(audio)
+print(text)
